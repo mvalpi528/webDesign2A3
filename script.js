5f0cb75b0129f7449168fffe93022833e966fa5f
mv <S3907456@student.rmit.edu.au>
2023-08-11 20:10:02 +1000

    adding the first shoelace UI components

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Code for image popups
 
 // Store all gallery images in a variable
+// Selects all images that are contained within elements that have the class "image-container"
 const galleryImages = document.querySelectorAll(".image-container img");
 
 function createImagePopUp(imageCollection) {
